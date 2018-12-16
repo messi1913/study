3b318ded4d024d8b886c9575ffa55153c27e52e5
sangmessi <messi1913@gmail.com>
2018-12-16 23:41:22 +0900

    First commit for rest-api lesson

---
+++ src/test/java/me/sangmessi/restapi/events/EventTest.java
@@ -0,0 +1,33 @@
+package me.sangmessi.restapi.events;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class EventTest {
+
+    @Test
+    public void builder(){
+        Event event = Event.builder()
+                .name("Inflearn Spring REST Api")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    public void javaBean(){
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+
+}
